c6302d82922b5913a716729a5a66c51abce6c7ea
Dion Fananie <dion.fananie>
2024-01-06 13:09:52 +0700

    feat(board): init board

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import Board from "./modules/Board";
+
+function App() {
+  //draggable puzzle is the idea, not yet finished as you can probably tell
+  //copied and pasted this js from w3school
+  //feel free to give suggestions to any part of the code and tweet me @ilovepeoplez ! ^^
+
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
